7002052d82e2f6360a72a280d2c6376a3d66a3d0
JuanCompaire <juancompaire2004@gmail.com>
2023-11-08 11:10:38 +0100

    terminado ejercico U5A1
    Signed-off-by: JuanCompaire <juancompaire2004@gmail.com>

--- SPRINBOOT/EJERCICIOS/U5A1/spring-boot-2-jdbc-with-h2/src/main/java/com/in28minutes/springboot/rest/example/springboot2jdbcwithh2/SpringBoot2JdbcWithH2Application.java
+++ SPRINBOOT/EJERCICIOS/U5A1/spring-boot-2-jdbc-with-h2/src/main/java/com/in28minutes/springboot/rest/example/springboot2jdbcwithh2/SpringBoot2JdbcWithH2Application.java
@@ -1,13 +1,47 @@
 package com.in28minutes.springboot.rest.example.springboot2jdbcwithh2;
 
+import com.in28minutes.springboot.rest.example.springboot2jdbcwithh2.student.Student;
+import com.in28minutes.springboot.rest.example.springboot2jdbcwithh2.student.StudentJdbcRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot2JdbcWithH2Application {
+public class SpringBoot2JdbcWithH2Application implements CommandLineRunner {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	StudentJdbcRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot2JdbcWithH2Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		logger.info("Student id 10001 -> {}", repository.findByid(10001L));
+
+		logger.info("Student passport_number e1234567-> {}", repository.findBypassport_number("e1234567"));
+
+
+		logger.info("inserting -> {}", repository.insert(new Student(10010L, "john", "a1234657")));
+
+		logger.info("inserting -> {}", repository.insert(new Student(10020L, "marcos", "b1234657")));
+
+
+		logger.info("update 10001 -> {}", repository.update(new Student(10001L, "name-updated", "new-passport")));
+
+		repository.deleteByid(10002L);
+
+		repository.deletedBypassport_number("b1234657");
+
+		logger.info("all users 2 -> {}", repository.findAll());
+
+
+	}
 }
---
+++ SPRINBOOT/EJERCICIOS/U5A1/spring-boot-2-jdbc-with-h2/src/main/java/com/in28minutes/springboot/rest/example/springboot2jdbcwithh2/student/StudentJdbcRepository.java
@@ -0,0 +1,72 @@
+package com.in28minutes.springboot.rest.example.springboot2jdbcwithh2.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+
+@Repository
+public class StudentJdbcRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    static class StudentRowmapper implements RowMapper<Student>{
+        @Override
+        public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Student student = new Student();
+            student.setId(rs.getLong("id"));
+            student.setName(rs.getString("name"));
+            student.setPassportNumber(rs.getString("passport_number"));
+            return student;
+        }
+    }
+
+    public List <Student> findAll() {
+        return jdbcTemplate.query("select * from student", new StudentRowmapper());
+    }
+
+    public Student findByid(Long id) {
+        return jdbcTemplate.queryForObject("select * from student where id=?", new Object[] {
+                        id
+                },
+                new BeanPropertyRowMapper<Student>(Student.class));
+    }
+
+    public Student findBypassport_number(String passport_number){
+        return jdbcTemplate.queryForObject("select * from student where passport_number=?", new Object[]{
+            passport_number
+        },
+        new BeanPropertyRowMapper<Student>(Student.class));
+    }
+
+    public int deleteByid(Long id) {
+        return jdbcTemplate.update("delete from student where id=?", new Object[] {
+                id
+        });
+    }
+
+    public int deletedBypassport_number(String passport_number){
+        return jdbcTemplate.update("delete from student where passport_number=?", new Object[]{
+                passport_number
+        });
+    }
+
+    public int insert(Student student) {
+        return jdbcTemplate.update("insert into student (id, name, passport_number) " + "values(?,  ?, ?)",
+                new Object[] {
+                        student.getId(), student.getName(), student.getPassportNumber()
+                });
+    }
+
+    public int update(Student student) {
+        return jdbcTemplate.update("update student " + " set name = ?, passport_number = ? " + " where id = ?",
+                new Object[] {
+                        student.getName(), student.getPassportNumber(), student.getId()
+                });
+    }
+    }
