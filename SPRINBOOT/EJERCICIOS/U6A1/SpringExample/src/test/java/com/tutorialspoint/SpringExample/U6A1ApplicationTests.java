1086d7a19703f1bbd5b32184ed007b9a702bb8c2
JuanCompaire <juancompaire2004@gmail.com>
2023-11-22 13:41:54 +0100

    ejercicio u6a1

---
+++ SPRINBOOT/EJERCICIOS/U6A1/SpringExample/src/test/java/com/tutorialspoint/SpringExample/U6A1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorialspoint.SpringExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U6A1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
