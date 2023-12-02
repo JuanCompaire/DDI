f011e544b39885ba7cdd838fcb5ac5c54e33e2d7
JuanCompaire <117914634+JuanCompaire@users.noreply.github.com>
2023-12-02 11:03:54 +0100

    u6a2 fisnish

---
+++ SPRINBOOT/EJERCICIOS/U6A2/student-service/student-service/src/main/java/com/in28minutes/springboot/studentservice/model/Todo.java
@@ -0,0 +1,14 @@
+package package com.in28minutes.springboot.studentservice.model;
+
+
+import java.util.Date;
+
+public class Todo {
+    private int id;
+    private String user;
+    private String desc;
+    private Date targetDate;
+    private boolean isDone;
+
+//Getters, Setters, Constructors, toString, equals and hash code
+}
